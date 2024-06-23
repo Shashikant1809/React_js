c4c0719ad59c0f49dab16ff5adb4f230bf546565
Shashikant1809 <shashikantverma1809@gmail.com>
2024-06-23 14:33:34 +0530

    learned about if else and ternary operator

--- src/App.js
+++ src/App.js
@@ -8,8 +8,29 @@ import Student from "./components/Student";
 // for single fucntion we can write any name during import and can use that name during rendering
 
 function App() {
+  // let login = true;
+
+  // if (login == true) {
+  //   return " i am learning reat from web3Mantra";
+  // } else {
+  //   return "i am leaning nothing ";
+  // }
+
+  // we can't use directly if else in JSX
+
+  //
+
+  // so we use ternary operator
+
   return (
     <div className="App">
+      {/* how to use if else in JSX  */}
+
+      {(() => {
+        {
+          /* write if else  */
+        }
+      })()}
       <Student />
     </div>
   );
